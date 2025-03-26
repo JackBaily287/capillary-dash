eab6dc961adb10a911a5ceb485bd53a8b48f6f61
Murtaza Abbasali <murtazaabbasali@Murtazas-MacBook-Pro.local>
2025-03-26 16:20:32 +0000

    Update app.run for Render deployment

--- capillary_dash.py
+++ capillary_dash.py
@@ -140,5 +140,7 @@ def update_graphs(h_cm):
 
     return fig_meniscus, fig_theta
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(host='0.0.0.0', port=int(os.environ.get('PORT', 8050)), debug=True)
