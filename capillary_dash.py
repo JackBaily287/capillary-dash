ddc7ac0a65fc024cc620ffb72ca4c6b42c2987d9
Murtaza Abbasali <murtazaabbasali@Murtazas-MacBook-Pro.local>
2025-03-26 23:20:23 +0000

    Fix missing parenthesis in run_servering

--- capillary_dash.py
+++ capillary_dash.py
@@ -42,6 +42,7 @@ app.layout = html.Div([
             value=1.0,
             marks={i: f"{i} cm" for i in np.arange(0, 3.1, 0.5)},
             tooltip={"placement": "bottom", "always_visible": True}
+            updatemode='drag'  # ← This is the key line
         )
     ], style={"width": "70%", "margin": "auto"}),
 
